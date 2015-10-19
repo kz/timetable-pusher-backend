c9878e1680e562d1f8eab34bb69a4c122f9901f9
Kelvin Zhang <me@iamkelv.in>
2015-10-19 18:15:06 +0100

    Add models

---
+++ app/Job.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace TimetablePusher;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    //
+}
---
+++ app/Pin.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace TimetablePusher;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pin extends Model
+{
+    //
+}
