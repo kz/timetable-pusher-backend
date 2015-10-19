1e86a35fb5d828c6111e5f4f4679f14a8493aef9
Kelvin Zhang <me@iamkelv.in>
2015-10-19 22:41:33 +0100

    Update migration =

--- database/migrations/2015_10_19_164501_create_timetables_table.php
+++ database/migrations/2015_10_19_164501_create_timetables_table.php
@@ -14,6 +14,8 @@ class CreateTimetablesTable extends Migration
     {
         Schema::create('timetables', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
             $table->string('name');
             $table->string('data');
             $table->timestamps();
