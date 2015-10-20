5a39a09a1e15fe456e5184807a2eeea518bab712
Kelvin Zhang <me@iamkelv.in>
2015-10-20 12:33:19 +0100

    Add token generation to controller

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -2,85 +2,30 @@
 
 namespace TimetablePusher\Http\Controllers;
 
+use Auth;
 use Illuminate\Http\Request;
 use TimetablePusher\Http\Requests;
 use TimetablePusher\Http\Controllers\Controller;
+use TimetablePusher\TimetablePusher\Token;
 
 class TokenController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
      * @return \Illuminate\Http\Response
+     * @internal param Request $request
+     * @internal param int $id
      */
-    public function update(Request $request, $id)
+    public function update()
     {
-        //
-    }
+        // Generate new token
+        $token = new Token();
+        $apiToken = $token->generateUnique();
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $user = Auth::user();
+        $user->api_token = $apiToken;
+        $user->update();
+
+        return redirect('dashboard')->with('success', 'Your API token has been regenerated.');
     }
 }
