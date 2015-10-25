eb60bd737877b510bb5dcd6748e35515bab9d514
Kelvin Zhang <me@iamkelv.in>
2015-10-25 19:12:57 +0000

    Add exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,7 +6,7 @@ use Exception;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Bugsnag\BugsnagLaravel\BugsnagExceptionHandler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
