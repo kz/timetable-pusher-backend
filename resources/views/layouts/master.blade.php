2cc4a65f37b143a18fe6ac9f1404f1c66e19c1fb
Kelvin Zhang <github@iamkelv.in>
2017-05-17 21:35:06 +0100

    Update Google Fonts URL to HTTPS

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <!--Import Google Icon Font-->
-    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/css/materialize.min.css">
     <link rel="stylesheet" href="/css/custom.css">
